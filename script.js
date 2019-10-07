be4288559d511ac4f9f79a63dc19d4c6ae5b16d7
mankeip3531 <44926837+mankeip3531@users.noreply.github.com>
2019-10-07 09:37:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Alan Po"
+let userName = "mankeip3531";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
